588fa2cdebc1acd026073673ebde27ea24ee362f
anaswal <anaswal321@gmail.com>
2023-08-08 11:38:53 +0800

    Adding option function

--- script.js
+++ script.js
@@ -8,16 +8,32 @@ const computerSelection = getComputerChoice();
 console.log(computerSelection);
 
 function playRound(computerSelection, playerSelection) {
-  if (computerSelection == "paper") {
-    return `You lose! ${computerSelection} beats the ${playerSelection}`;
-  } else if (computerSelection == "scissor") {
-    return `You win! ${playerSelection} beats the ${computerSelection}`;
-  } else {
-    return `Draw`;
+  switch (playerSelection) {
+    case "rock":
+      return computerSelection === "paper"
+        ? `You lose! ${computerSelection} beats the ${playerSelection}`
+        : computerSelection === "scissor"
+        ? `You win! ${playerSelection} beats the ${computerSelection}`
+        : "Draw!";
+
+    case "paper":
+      return computerSelection === "scissor"
+        ? `You lose! ${computerSelection} beats the ${playerSelection}`
+        : computerSelection === "rock"
+        ? `You win! ${playerSelection} beats the ${computerSelection}`
+        : "Draw!";
+    case "scissor":
+      return computerSelection === "rock"
+        ? `You lose! ${computerSelection} beats the ${playerSelection}`
+        : computerSelection === "paper"
+        ? `You win! ${playerSelection} beats the ${computerSelection}`
+        : "Draw!";
+    default:
+      return "Wrong choice!";
   }
 }
 
-const playerSelectionMe = "rock";
+const playerSelectionMe = prompt("Rock/Paper/Scissor :").toLowerCase();
 
 const result = playRound(computerSelection, playerSelectionMe);
 console.log(result);
